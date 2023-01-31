b8de48bcc4aaca1869bd2d3e10d1c08f042405c9
JuanmaBigo <juanmacuerntatanlet@gmail.com>
2023-01-31 17:01:10 -0300

    added cards generated via Javascript, and included filters to sort the cards by date

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+const cardContainer = document.querySelector('.card-container');
+let cardsInfo = data.events;
+let cards = '';
+
+for (let cardInfo of cardsInfo) {
+    cards += `<div class="card rounded-0" style="width: 18rem;">
+    <img src="${cardInfo.image}" class="card-img-top p-3" alt="...">
+    <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">${cardInfo.name}</h5>
+        <p class="card-text">${cardInfo.description}</p>
+        <div class="d-flex container-fluid align-self-end justify-content-between align-items-center">
+            <p class="mb-0">Price: US$${cardInfo.price}</p>
+            <a href="./assets/html/details.html" class="btn btn-primary">See More</a>
+        </div>
+    </div>
+</div>`
+}
+
+cardContainer.innerHTML = cards;
---
+++ assets/js/upcoming.js
@@ -0,0 +1,40 @@
+function formatDate(date) {
+    let dateArray = date.split("-");
+    let year = dateArray[0];
+    let month = dateArray[1];
+    let day = dateArray[2];
+    let dateObject = new Date(year, month, day);
+    return dateObject;
+}
+
+
+let cardsInfo = data.events;
+let currentDate = data.currentDate;
+
+
+let upcomingCards = [];
+for (let card of cardsInfo) {
+    if (formatDate(card.date) >= formatDate(currentDate)) {
+        upcomingCards.push(card);
+    }
+}
+
+
+const cardContainer = document.querySelector('.card-container');
+let cards = '';
+
+for (let cardInfo of upcomingCards) {
+    cards += `<div class="card rounded-0" style="width: 18rem;">
+    <img src="${cardInfo.image}" class="card-img-top p-3" alt="...">
+    <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title">${cardInfo.name}</h5>
+        <p class="card-text">${cardInfo.description}</p>
+        <div class="d-flex container-fluid align-self-end justify-content-between align-items-center">
+            <p class="mb-0">Price: US$${cardInfo.price}</p>
+            <a href="../html/details.html" class="btn btn-primary">See More</a>
+        </div>
+    </div>
+</div>`
+}
+
+cardContainer.innerHTML = cards;
